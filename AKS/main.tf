ee4d1543e437e597848bd80d9ab69c88798bbf08
RekhuGopal <vrchinnarathod@gmail.com>
2023-10-29 08:07:46 +0530

    Updated

--- AKS/main.tf
+++ AKS/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_D1_v2"
+    vm_size    = "Standard_D2s_v3"
     node_count = var.node_count
   }
   linux_profile {
